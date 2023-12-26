07f89a7f1d8b1d7d7f80ea97ceed78d08797b985
ramli fariji zamhuri <ramlif828@gmail.com>
2023-12-26 10:01:31 +0700

    darkmode

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 function myFunction() {
-    var x = document.getElementById("myNav");
-    if (x.className === "navbar-nav") {
-      x.className += " responsive";
-    } else {
-      x.className = "navbar-nav";
-    }
+  var x = document.getElementById("myNav");
+  if (x.className === "navbar-nav") {
+    x.className += " responsive";
+  } else {
+    x.className = "navbar-nav";
   }
+}
+
+
+
+function dark() {
+  let sun = document.getElementById("sun")
+  let moon = document.getElementById("moon")
+  let body = document.body;
+  body.classList.toggle("dark")
+  Text.style.color = "white"
+    moon.style.display = "inline"
+}
