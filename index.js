bcb794894100a9a1668c09721290a79935779bfa
Ramli Fariji Zamhuri <ramlif828@gmail.com>
2026-06-28 15:09:37 +0700

    DOM manipulation

--- index.js
+++ index.js
@@ -8,13 +8,12 @@ function myFunction() {
 }
 
 
-
 function dark() {
   let body = document.body;
   let sun = document.getElementById("sun")
   let moon = document.getElementById("moon")
   body.classList.toggle("dark")
-  
+
   if (body.className !== "dark") {
     sun.style.display = "none";
     moon.style.display = "inline";
@@ -23,5 +22,63 @@ function dark() {
     sun.style.display = "inline"
   }
 }
+
+const about = [
+  {
+    title: "Manisan",
+    desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Deserunt placeat, et minima sunt reiciendis recusandae perferendis fugiat odio illo! Fuga consequatur quod, quo asperiores magni ea.",
+    image: "/logo/manisan.jpeg"
+  },
+  {
+    title: "Tauco",
+    desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Deserunt placeat, et minima sunt reiciendis recusandae perferendis fugiat odio illo! Fuga consequatur quod, quo asperiores magni ea.",
+    image: "/logo/tauco.jpeg"
+  },
+  {
+    title: "Moci",
+    desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Deserunt placeat, et minima sunt reiciendis recusandae perferendis fugiat odio illo! Fuga consequatur quod, quo asperiores magni ea.",
+    image: "/logo/moci.jpg"
+  },
+  {
+    title: "Laksa Cianjur",
+    desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Deserunt placeat, et minima sunt reiciendis recusandae perferendis fugiat odio illo! Fuga consequatur quod, quo asperiores magni ea.",
+    image: "/logo/laksa.jpg"
+  },
+  {
+    title: "Bandrek",
+    desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Deserunt placeat, et minima sunt reiciendis recusandae perferendis fugiat odio illo! Fuga consequatur quod, quo asperiores magni ea.",
+    image: "/logo/Bandrek.jpg"
+  },
+  {
+    title: "Geco Cianjur",
+    desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Deserunt placeat, et minima sunt reiciendis recusandae perferendis fugiat odio illo! Fuga consequatur quod, quo asperiores magni ea.",
+    image: "/logo/Geco.jpg"
+  },
+  {
+    title: "Batagor Cianjur",
+    desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Deserunt placeat, et minima sunt reiciendis recusandae perferendis fugiat odio illo! Fuga consequatur quod, quo asperiores magni ea.",
+    image: "/logo/Batagor-Cianjur.jpg"
+  },
+  {
+    title: "Nasi Pepes",
+    desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Deserunt placeat, et minima sunt reiciendis recusandae perferendis fugiat odio illo! Fuga consequatur quod, quo asperiores magni ea.",
+    image: "/logo/NasiPepes.jpg"
+  }
+];
+
+const products = document.getElementById("products");
+
+about.forEach(item => {
+  products.innerHTML += `
+    <div class="product-items">
+      <img src="${item.image}" alt="${item.title}" width="420" height="250">
+
+      <div class="desc">
+        <h1>${item.title}</h1>
+        <p>${item.desc}</p>
+      </div>
+    </div>
+  `;
+});
 
 
